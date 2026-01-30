76fbd4d68c83cda3644afe306a5746361afd982a
sidali <sidalialili694@gmail.com>
2026-01-30 22:27:21 +0100

    erreur

---
+++ logic/_init_.py
@@ -0,0 +1,8 @@
+# logic/__init__.py
+
+from logic.predicate import Predicate
+from logic.fact import Fact
+from logic.default_rule import DefaultRule
+from logic.extension import Extension
+from logic.theory import DefaultTheory
+from logic.reasoner import Reasoner
